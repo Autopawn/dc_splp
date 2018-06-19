b6dcf562c9ad88e8e78f675a88f9c8401ec32b28
Francisco Casas <franciscojacb@gmail.com>
2018-06-19 14:00:42 -0400

    Changed problem collection.

--- solve_problems.sh
+++ solve_problems.sh
@@ -7,27 +7,25 @@ bash generate_problems.sh $1
 
 qsub -N pm_lpsolve solve_lpsolve.sh -F "5 pm" || \
     bash solve_lpsolve.sh "2" "pm"
-
 qsub -N splp_lpsolve solve_lpsolve.sh -F "5 splp" || \
     bash solve_lpsolve.sh "2" "splp"
 
+# qsub -N pm_dsa100vr400 solve_dsa.sh -F "5 pm 100 500" || \
+#     bash solve_dsa.sh "2" "pm" "100" "500"
+# qsub -N splp_dsa100vr400 solve_dsa.sh -F "5 splp 100 500" || \
+#     bash solve_dsa.sh "2" "splp" "100" "500"
 
-qsub -N pm_dsa200 solve_dsa.sh -F "5 pm 200" || \
-    bash solve_dsa.sh "2" "pm" "200"
+qsub -N pm_dsa100 solve_dsa.sh -F "4 pm 100" || \
+    bash solve_dsa.sh "1" "pm" "100"
+qsub -N splp_dsa100 solve_dsa.sh -F "4 splp 100" || \
+    bash solve_dsa.sh "1" "splp" "100"
 
-qsub -N splp_dsa200 solve_dsa.sh -F "5 splp 200" || \
-    bash solve_dsa.sh "2" "splp" "200"
+qsub -N pm_dsa200vr500 solve_dsa.sh -F "5 pm 200 500" || \
+    bash solve_dsa.sh "2" "pm" "200" "500"
+qsub -N splp_dsa200vr500 solve_dsa.sh -F "5 splp 200 500" || \
+    bash solve_dsa.sh "2" "splp" "200" "500"
 
-
-qsub -N pm_dsa200vr400 solve_dsa.sh -F "5 pm 200 400" || \
-    bash solve_dsa.sh "2" "pm" "200" "400"
-
-qsub -N splp_dsa200vr400 solve_dsa.sh -F "5 splp 200 400" || \
-    bash solve_dsa.sh "2" "splp" "200" "400"
-
-
-qsub -N pm_dsa400vr800 solve_dsa.sh -F "5 pm 400 800" || \
-    bash solve_dsa.sh "2" "pm" "400" "800"
-
-qsub -N splp_dsa400vr800 solve_dsa.sh -F "5 splp 400 800" || \
-    bash solve_dsa.sh "2" "splp" "400" "800"
+qsub -N pm_dsa400vr1000 solve_dsa.sh -F "5 pm 400 1000" || \
+    bash solve_dsa.sh "2" "pm" "400" "1000"
+qsub -N splp_dsa400vr1000 solve_dsa.sh -F "5 splp 400 1000" || \
+    bash solve_dsa.sh "2" "splp" "400" "1000"
